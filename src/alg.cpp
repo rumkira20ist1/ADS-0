4c7344d3d86d42c82d0ec3dd31433bedd00375be
rumkira20ist1 <78916886+rumkira20ist1@users.noreply.github.com>
2021-02-25 11:32:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,10 @@
 // Copyright 2021 NNTU-CS
-
-int gcd(int a, int b){
-int c;
-while (b) {
-c=a%b;
-a=b;
-b=c;
-}
-return a;
-}
+ int gcd(int a, int b) {
+   int c;
+   while (b) {
+      c = a % b;
+      a = b;
+      b = c;        
+   }
+   return a;
+ }
